63738e3ecdcc35f2d6c9c25c5fffae47940863f7
xomaiya <xomaiya@gmail.com>
2024-05-14 13:19:35 +0200

    Delete run-files

--- convert_TIMIT_to_spectras.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-
-export MATLABPATH=../NSLtools
-
-matlab -nodisplay -nosplash -nodesktop -r "convert_TIMIT_to_spectras('$1', '$2');exit;"
--- run_dem.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-
-export MATLABPATH=../spm12
-
-#Executable
-matlab -nodisplay -nosplash -nodesktop -r "run('run_dem.m');exit;"
